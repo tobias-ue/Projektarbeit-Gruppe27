c78b1d58b95896d38a590ec0d90e0a886e761982
Julius <julius@happersberger.com>
2025-01-13 09:52:44 +0100

    3. ArrayList und Methode initObjekte() erstellt

--- src/main/java/org/example/KonzertfinderGUI.java
+++ src/main/java/org/example/KonzertfinderGUI.java
@@ -2,6 +2,8 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class KonzertfinderGUI extends JFrame {
     private JLabel lblEingabe;
@@ -33,12 +35,28 @@ public class KonzertfinderGUI extends JFrame {
     private JButton btAlleAnzeigen;
     private JPanel jpPanel;
 
+    private ArrayList<Konzert> konzertliste = new ArrayList<Konzert>();
+
     public KonzertfinderGUI() throws HeadlessException {
         setTitle("Konzertsuche");
         setContentPane(jpPanel);
         setSize(1200,500);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void initObjekte(){
+        LocalDateTime d1 =  LocalDateTime.of(2025,04,23,19,00);
+        Konzert init1 = new Konzert("Ed Sheeran", d1, "Pop", 45,true);
+        konzertliste.add(init1);
+
+        LocalDateTime d2 =  LocalDateTime.of(2025,07,02,20,30);
+        Konzert init2 = new Konzert("Linkin Park", d2, "Rock", 110,true);
+        konzertliste.add(init2);
+
+        LocalDateTime d3 = LocalDateTime.of(2025,03,19,21,00);
+        Konzert init3 = new Konzert("Eminem",d3,"HipHop",85,false);
+        konzertliste.add(init3);
     }
 
 }
